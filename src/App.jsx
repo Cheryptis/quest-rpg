be1dd7125945345842ade85bd9efc761cbb1644e
Cheryptis <jakob.krex@gmail.com>
2026-07-11 13:19:33 +0200

    Projekt erstellt

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Quest RPG</h1>
+      <p>Willkommen zu deinem Abenteuer!</p>
+      <button>Quest starten</button>
+    </div>
+  );
+}
+
+export default App;
